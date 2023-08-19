906e150b5cb515c4cfd1e1656d9877c1238b2db9
drowlink <pauloriveiro01@gmail.com>
2023-08-19 12:01:39 -0400

    first commit

---
+++ the_weather/weather/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=4a2fc37637d2bcd9f85f9b02c66ee6bd"
+    return render(request, 'weather/weather.html')
