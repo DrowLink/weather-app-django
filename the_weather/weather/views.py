6da1654dd6b22342c53f07e3fc1fa49045c5bca9
drowlink <pauloriveiro01@gmail.com>
2023-08-22 18:23:55 -0400

    refix

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# setting deserve
+
+
 # Create your views here.
 def index(request):
     url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=4a2fc37637d2bcd9f85f9b02c66ee6bd"
