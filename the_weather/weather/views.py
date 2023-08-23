8089c467440637392a9b02aad953b80e1733b3bb
drowlink <pauloriveiro01@gmail.com>
2023-08-23 13:42:08 -0400

    finished first part (comparator not yet)

--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -1,9 +1,40 @@
+import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 # setting deserve
 
 
 # Create your views here.
 def index(request):
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid=4a2fc37637d2bcd9f85f9b02c66ee6bd"
-    return render(request, 'weather/weather.html')
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=4a2fc37637d2bcd9f85f9b02c66ee6bd"
+    city = 'Caracas'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city,
+            'temperature': r['main']['temp'], 
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+    
+    print(weather_data)
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    return render(request, 'weather/weather.html', context)
